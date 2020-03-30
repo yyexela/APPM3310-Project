26366218d4ecd8f835805f29c9c49f0cc33cbe79
yyexela <yyexela@disroot.org>
2020-03-29 19:54:32 -0600

    Data structure changed from array to array of linked-lists

--- parse-csv.cc
+++ parse-csv.cc
@@ -6,20 +6,30 @@
 
 using namespace std;
 
-// Size of the sparse matrix: M x N
-#define M 3 
-#define N 3
+// User amount
+//#define USERS 480189
+#define USERS 2649429
 
-// 2D Initial Array of size M x N
-unsigned short arr[M][N];
+// Node for linked-list for the user-item matrix
+struct cell{
+    short rating;
+    int uid;
+    int item;
+    cell *next;
+};
 
 // Forward declarations
 void SparseLine(string line);
-void PrintSparse();
 void UIDMapLine(string line);
+void UpdateUser(int uid, int item, int rating);
+void PrintLLArr();
+void PrintCell(cell* cl);
 
 // Boolean used to print debug info
-const bool DEBUG = true;
+const bool DEBUG = false;
+const bool DEBUG_UID = false;
+
+int item_counter = 0;
 
 // Files to read from
 //const string SPARSE_FILE = "InputCSV/ex_sparse.csv";
@@ -29,6 +39,9 @@ const string UIDMAP_FILE = "InputCSV/userID_map.csv";
 
 // Hashmap for userID map
 unordered_map<int, int> uid_map;
+
+// User array of linked-lists
+cell* user[USERS];
 
 int main(int argc, char* argv[]){
     string msg = "Running parse-csv.cc";
@@ -40,6 +53,8 @@ int main(int argc, char* argv[]){
 
     // ifs is used to read files
     ifstream ifs;
+
+    cout << "Processing uid_map" << endl;
 
     // Open file, check if failed
     ifs.open(UIDMAP_FILE);
@@ -57,6 +72,9 @@ int main(int argc, char* argv[]){
     // Close UIDMAP_FILE
     ifs.close();
 
+    cout << "Processed uid_map" << endl;
+    cout << "Processing sparse_matrix file" << endl;
+
     // Open file, check if failed
     ifs.open(SPARSE_FILE);
     if(!ifs.is_open())
@@ -70,16 +88,95 @@ int main(int argc, char* argv[]){
             SparseLine(line);
     }
 
-    // Print parsed sparse matrix
-    PrintSparse();
-
     // Close SPARSE_FILE
     ifs.close();
 
-    // Check map contents
+    cout << "Processed sparse_matrix file" << endl;
+
+    PrintLLArr();
+
     cout << "Done processing" << endl;
     duration = (std::clock() - start) / (double) CLOCKS_PER_SEC;
     cout << "Time: " << duration << endl;
+}
+
+void PrintLLArr(){
+    for(int i = 0; i < USERS; ++i){
+        cell* tmp = user[i];
+        while(tmp != NULL){
+            PrintCell(tmp);
+            tmp = tmp->next;
+        }
+    }
+}
+
+void PrintCell(cell* cl){
+    cout << "user " << cl->uid << ", movie " << cl->item << ", rating " << cl->rating << endl;
+}
+
+void UpdateUser(int uid, int item, int rating){
+    if(item - item_counter == 100){
+        cout << item << endl;
+        item_counter = item;
+    }
+
+    //create the corresponding person
+    cell* new_user = new cell();
+    new_user->rating = rating;
+    new_user->item = item;
+    new_user->next = NULL;
+    new_user->uid = uid;
+
+    if(DEBUG) cout << "Adding user " << uid << ", item " << new_user->item << ", rating " << new_user->rating << endl;
+
+    //check if root is null
+    if(user[uid] == NULL){
+        //add user as head
+        if(DEBUG) cout << "Adding user as head (null LL)" << endl;
+        user[uid] = new_user;
+    } else {
+        //there exists a linkedlist, traverse until item is between 2 or last
+        cell* tmp = user[uid];
+        cell* tmp_prev = NULL;
+
+        if(DEBUG){
+            cout << "Initial tmp: ";
+            PrintCell(tmp);
+        }
+
+        while(  tmp->next != NULL && 
+                new_user->item > tmp->item){
+            tmp_prev = tmp;
+            tmp = tmp->next;
+            if(DEBUG) cout << "Iterated tmp" << endl;
+        }
+        // tmp->next == NULL OR new_user->item < tmp->item
+        if(new_user->item < tmp->item){
+            //check if root was tmp
+            if(tmp_prev == NULL){
+                if(DEBUG) cout << "Adding user as head (before tmp)" << endl;
+                //set new_user as head
+                user[uid] = new_user;
+                new_user->next = tmp;
+            } else {
+                //set new_user between tmp_prev and tmp
+                if(DEBUG) cout << "Adding user before tmp and after tmp_prev" << endl;
+                tmp_prev->next = new_user;
+                new_user->next = tmp;
+            }
+        } else {
+            //set new_user as tail
+            if(DEBUG) cout << "Adding user at end of LL" << endl;
+            tmp->next = new_user;
+            if(DEBUG){
+                cout << "tmp: ";
+                PrintCell(tmp);
+                cout << "tmp->next: ";
+                PrintCell(tmp->next);
+            }
+        }
+    }
+    if(DEBUG) cout << endl;
 }
 
 /* 
@@ -89,7 +186,7 @@ int main(int argc, char* argv[]){
  * Stores into the map like so: (key | value) = (X | Y)
  */
 void UIDMapLine(string line){
-    if(DEBUG) cout << "Parsing " << line << endl;
+    if(DEBUG && DEBUG_UID) cout << "Parsing " << line << endl;
 
     // Position of comma
     size_t pos = line.find(',', 0);
@@ -103,17 +200,17 @@ void UIDMapLine(string line){
 
     // First number: User
     temp = line.substr(0, pos);
-    if(DEBUG) cout << "pos: " << pos << ", substr: \"" << temp << "\"" << endl;
+    if(DEBUG && DEBUG_UID) cout << "pos: " << pos << ", substr: \"" << temp << "\"" << endl;
     uid_old = stoi(temp);
 
     // Second number: Item
     temp = line.substr(pos + 1, line.size() - pos - 2);
-    if(DEBUG) cout << "pos: " << pos << ", substr: \"" << temp << "\"" << endl;
+    if(DEBUG && DEBUG_UID) cout << "pos: " << pos << ", substr: \"" << temp << "\"" << endl;
     uid_new = stoi(temp);
 
     // Insert into hash map
     uid_map.insert(make_pair(uid_old,uid_new));
-    if(DEBUG) cout << "key " << uid_old << " value " << uid_map[uid_old] << endl;
+    if(DEBUG && DEBUG_UID) cout << "key " << uid_old << " value " << uid_map[uid_old] << endl << endl;
 }
 
 /* 
@@ -127,7 +224,7 @@ void SparseLine(string line){
     size_t pos_c = 0, pos_n;
 
     // Extracted values
-    int user, item, rating;
+    int uid, item, rating;
 
     // Temp string used for substr
     string temp;
@@ -135,10 +232,10 @@ void SparseLine(string line){
     if(DEBUG) cout << "Parsing " << line << endl;
     pos_n = line.find(',', pos_c);
 
-    // First number: User
+    // First number: uid
     temp = line.substr(pos_c, pos_n - pos_c);
     if(DEBUG) cout << "pos_c: " << pos_c << ", pos_n: "<< pos_n << ", substr: \"" << temp << "\"" << endl;
-    user = stoi(temp);
+    uid = stoi(temp);
 
     // Second number: Item
     pos_c = pos_n;
@@ -150,21 +247,13 @@ void SparseLine(string line){
     // Third number: Rating
     pos_c = pos_n;
     temp = line.substr(pos_n + 1, line.size() - pos_n - 2); // -2 needed to get rid of funky newline
-    if(DEBUG) cout << "pos_c: " << pos_c << ", pos_n: "<< pos_n << ", substr: \"" << temp << "\"" << endl << endl;
+    if(DEBUG) cout << "pos_c: " << pos_c << ", pos_n: "<< pos_n << ", substr: \"" << temp << "\"" << endl;
     rating = stoi(temp);
 
-    // Update array
-    arr[uid_map.at(user) - 1][item - 1] = rating;
-}
+    if(DEBUG) cout << "Parsed into: " << uid << "," << item << "," << rating << endl;
+    //if(DEBUG) cout << "uid " << uid << "->" << uid_map.at(uid) << endl;
+    //uid = uid_map.at(uid);
 
-/*
- * Prints an array sized MxN, assuming
- * it is a user-item-rating matrix (outputs start at 1, not 0)
- */
-void PrintSparse(){
-    for(int i = 0; i < M; ++i){
-        for(int j = 0; j < N; ++j){
-            if(DEBUG) cout << "arr[" << i + 1 << "][" << j + 1 << "] = " << arr[i][j] << endl;
-        }
-    }
+    // Update array
+    UpdateUser(uid, item, rating);
 }
